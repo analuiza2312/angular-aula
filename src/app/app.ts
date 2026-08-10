f00c48b78fa3dcfeb3d99e7869feda4482ccc7e3
ana luiza <analuiza.rochacoelho09@gmail.com>
2026-08-10 11:28:07 -0300

    aula

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { Produto } from './features/produtos/produto/produto';
-import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
+
 
 @Component({
-  selector: 'app-root',
-  imports: [ ListaProdutos ],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+imports: [RouterOutlet, RouterLink],
+templateUrl: './app.html',
+styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('meu-primeiro-app');
+protected readonly title = signal('meu-primeiro-app');
 }
